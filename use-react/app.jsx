f9c09c8b4b82e7abfb337d899400ae110390dad3
2betop <liaoxuezhi@icloud.com>
2015-10-09 16:25:55 +0800

    add jsx file demo

---
+++ use-react/app.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+
+// tutorial1.js
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+
+React.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
