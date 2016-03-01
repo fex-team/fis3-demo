fbc60c5d92c95bff58345ff1eb750b01f86201c4
Sighless <hefangshi@gmail.com>
2016-03-01 13:31:55 +0800

    fix typo

--- use-sass/fis-conf.js
+++ use-sass/fis-conf.js
@@ -1,7 +1,7 @@
-//npm install -g fis-parser-sass
+//npm install -g fis-parser-node-sass
 fis.match('**/*.scss', {
     rExt: '.css', // from .scss to .css
     parser: fis.plugin('node-sass', {
-        //fis-parser-sass option
+        //fis-parser-node-sass option
     })
 });
