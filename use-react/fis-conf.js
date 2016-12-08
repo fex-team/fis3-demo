c194f99ee588e1da0aa8b9e6f2422f08cd9784b3
hefangshi <hefangshi@baidu.com>
2016-12-08 11:00:38 +0800

    use babel 6.x

--- use-react/fis-conf.js
+++ use-react/fis-conf.js
@@ -1,8 +1,11 @@
-fis.match('/components/**.js', {
+fis.match('{app.jsx,/node_modules/**.js}', {
     isMod: true
 });
 
 fis.hook('commonjs');
+fis.hook('node_modules', {
+    'shimProcess': false
+})
 
 fis.match('::package', {
     postpackager: fis.plugin('loader')
@@ -10,7 +13,7 @@ fis.match('::package', {
 
 // 编译所有后缀为 jsx 的文件为 js
 fis.match('{*.jsx,*:jsx}', {
-    parser: fis.plugin('babel-5.x', {
+    parser: fis.plugin('babel-6.x', {
         sourceMaps: true
     }),
     rExt: '.js'
